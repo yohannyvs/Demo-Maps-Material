0403737ff946d73bf0f2149424b8736f0074376b
yohannyvs <yohannyvs@gmail.com>
2018-07-18 18:35:27 -0600

    fin del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// Material Animation
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
+// Angular Google Maps
+import { AgmCoreModule } from '@agm/core';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
 
 @NgModule({
+  entryComponents: [
+    MapaEditarComponent
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBvvohOR2YB0mrsm4x3uZp1oIKo0FwFEio'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
